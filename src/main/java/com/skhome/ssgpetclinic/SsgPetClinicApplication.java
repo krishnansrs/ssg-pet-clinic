1b93aa87039e6d38094121f596e15b3afa0c9b18
Santhana Krishnan Ramasamy <krishnan.srs@gmail.com>
2020-09-06 12:47:25 -0400

    Initial Project Add

---
+++ src/main/java/com/skhome/ssgpetclinic/SsgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.skhome.ssgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skhome/ssgpetclinic/SsgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhome.ssgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
