7f758f6c84ddf8a371210b98921850ac63c5bca8
Santhana Krishnan Ramasamy <krishnan.srs@gmail.com>
2020-09-08 14:34:04 -0400

    Refactored crud service. Closes #11.

--- pet-clinic-data/src/main/java/com/skhome/ssgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/skhome/ssgpetclinic/services/VetService.java
@@ -1,14 +1,11 @@
 package com.skhome.ssgpetclinic.services;
 
 import com.skhome.ssgpetclinic.model.Owner;
+import com.skhome.ssgpetclinic.model.Pet;
 import com.skhome.ssgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends  CrudService<Vet, Long>{
 
 }
